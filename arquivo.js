9bb76d5a65f849f989f73f1ed93514adf8397f77
Germano <vini010219@gmail.com>
2023-05-22 16:21:48 -0300

    ProjetoIA

--- arquivo.js
+++ arquivo.js
@@ -1,4 +1,9 @@
-function exibirElemento(elementId) {
-    var elemento = document.getElementById(elementId).querySelector('.sigla').innerHTML;
-    document.getElementById('elementoSelecionado').innerHTML = "Elemento selecionado: " + elemento;
-}
+window.onload = function() {
+    var celulas = document.getElementsByTagName('td');
+    for (var i = 0; i < celulas.length; i++) {
+        celulas[i].onclick = function() {
+            var nome = this.querySelector('.nome').innerHTML;
+            document.getElementById('elementoSelecionado').innerHTML = "Elemento selecionado: <br><br>" + nome;
+        };
+    }
+};
