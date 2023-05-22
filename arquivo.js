c27ff383738a63ff7c7f94095e455a0b362a6c4e
Iara <iaravaneti9@gmail.com>
2023-05-22 20:50:48 -0300

    mudancas

--- arquivo.js
+++ arquivo.js
@@ -1,15 +1,28 @@
 let qntElementosPress = 0;
 let elementosPressCSS = [];
+let elementosJson;
 
-window.onload = function () {
+async function pegarJSON() {
+  try {
+    const response = await fetch('elementos.json');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log('Ocorreu um erro ao carregar o arquivo JSON:', error);
+  }
+}
+
+window.onload = async function() {
+  elementosJson = await pegarJSON();
+    console.log(elementosJson["H"].adjacentes)
   var celulas = document.getElementsByTagName("td");
   for (var i = 0; i < celulas.length; i++) {
-    celulas[i].onclick = function () {
+    celulas[i].onclick = function() {
       if (qntElementosPress < 2) {
-        elementosPressCSS.push(this)
+        elementosPressCSS.push(this);
         this.classList.add("elementos-pressionados");
         qntElementosPress++;
-      }else{
+      } else {
         elementosPressCSS[0].classList.remove("elementos-pressionados");
         elementosPressCSS.shift();
         elementosPressCSS.push(this);
@@ -17,12 +30,10 @@ window.onload = function () {
       }
       let nomeInicial = elementosPressCSS[0].querySelector(".nome").innerHTML;
       document.getElementById("elementoSelecionadoInicial").innerHTML = "Elemento Inicial: <br><br>" + nomeInicial;
-      if(elementosPressCSS.length==2){
+      if (elementosPressCSS.length == 2) {
         let nomeFinal = elementosPressCSS[1].querySelector(".nome").innerHTML;
         document.getElementById("elementoSelecionadoFinal").innerHTML = "Elemento Final: <br><br>" + nomeFinal;
       }
-    //   var sigla = this.querySelector(".siglacor").innerHTML;
-    //   document.getElementById("elementoSelecionadoInicial").innerHTML += nomeInicial;
     };
   }
 };
