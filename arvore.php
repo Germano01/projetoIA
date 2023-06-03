e01d7f4d4f3319857d9a6f66c93ee12fa661f087
Isabella <isabella-arj@hotmail.com>
2023-06-03 12:24:45 -0300

    primeira tentativa criar a arvore

---
+++ arvore.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- <link rel="icon" href="caminho/para/o/seu/icone.ico" type="image/x-icon"> -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
+	<link rel="stylesheet" type="text/css" href="CSS/arvore_style.css">
+	<link rel="stylesheet" type="text/css" href="CSS/style.css">
+	<script src="JS/arvore.js"></script>
+	<title>Heurística - Árvore</title>
+</head>
+<body>
+	<div class="menu">
+    	<ul>
+      		<li><a href="./arvore.php"><i class="fas fa-robot"></i> MATERIAL</a></li>
+      		<li><a href="#"><i class="fas fa-calculator"></i> CÁLCULO</a></li>
+    		<li><a href="#"><i class="fas fa-user"></i> GRUPO</a></li>
+    	</ul>
+  	</div>
+
+	<h1>ÁRVORE DE BUSCA</h1>
+	<div class="separador"></div>
+
+	<div id="arvore">
+		
+	</div>
+</body>
+</html>
