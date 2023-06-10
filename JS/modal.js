69d9ee14331adadab1e7a59c21367361e9f9d40b
Iara <iaravaneti9@gmail.com>
2023-06-10 11:26:27 -0300

    scrool centralizado

--- JS/modal.js
+++ JS/modal.js
@@ -10,6 +10,13 @@ function openModal(){
   }
   modal.classList.add('active')
   carregaArvore()
+
+  //Scrool fica cdentralizado ao abri o pop up
+  var janela = document.querySelector('.janela');
+  var metadeLargura = janela.scrollWidth / 2; //metade do tamanho da div janela
+  var metadeVisivel = janela.clientWidth / 2; //metade do tamanho da div que está vísivel
+  var posicaoInicial = metadeLargura - metadeVisivel; //subtrai os dois para que o scroll fique no meio
+  janela.scrollLeft = posicaoInicial;
 }
 
 function closeModal(){
